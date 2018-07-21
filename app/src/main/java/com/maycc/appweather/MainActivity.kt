8cf5c011a61b37f27186cdffa45fe645acea65b0
= <=>
2018-07-21 15:12:22 -0500

    renombrar paquete

--- app/src/main/java/com/maycc/appweather/MainActivity.kt
+++ app/src/main/java/com/maycc/appweather/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.maycc.app_weather
+package com.maycc.appweather
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
--- app/src/main/java/com/maycc/appweather/WeatherActivity.kt
+++ app/src/main/java/com/maycc/appweather/WeatherActivity.kt
@@ -1,4 +1,4 @@
-package com.maycc.app_weather
+package com.maycc.appweather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
