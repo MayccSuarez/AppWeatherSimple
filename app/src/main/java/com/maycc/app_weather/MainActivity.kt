5186dcc79a9aa5e24f1972564821ae6347fac31c
= <=>
2018-07-21 15:05:34 -0500

    al hacer click en el botón del clima de madrid ir a la WeatherActivity

--- app/src/main/java/com/maycc/app_weather/MainActivity.kt
+++ app/src/main/java/com/maycc/app_weather/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.maycc.app_weather
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addListenerBtnMadridWeather()
+    }
+
+    private fun addListenerBtnMadridWeather() {
+        btnMadridWeather.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, WeatherActivity::class.java)
+
+            startActivity(intent)
+        })
     }
 }
